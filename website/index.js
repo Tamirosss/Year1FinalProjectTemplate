1965145825c8ed619a1c6b2497e9cd50207dce6b
Tamirosss <tamiros3012@gmail.com>
2024-06-04 13:25:07 +0300

    nnnnn

--- website/index.js
+++ website/index.js
@@ -15,21 +15,37 @@ let previews = await send("/getAppoitment");
 console.log(previews);
 for (let i = 0; i < previews.length; i++) {
     let button = document.getElementById((i + 1).toString());
-    if (previews[i].UserId != '') {
-        let button = document.getElementById((i + 1).toString());
-        button.style.backgroundColor = "red";
+    if (button.style.backgroundColor != "green" && button.style.backgroundColor != "red") {
+        button.onclick = function () {
+            window.location.href = "chack.html?day=" + (i+1);
+        }
+    }
+    if (previews[i].UserId == Cookies.get("id")) {
+        button.onclick = function () {
+            window.location.href = "cancel.html";
+        }
+    }
+    else{
+        button.onclick = function () {
+            
+        }
+    }
 
+    
+}
+for (let i = 0; i < previews.length; i++) {
+    let button = document.getElementById((i + 1).toString());
+    if (previews[i].UserId != '') {
+        button.style.backgroundColor = "red";
 
     }
     if (previews[i].UserId == Cookies.get("id")) {
         let button = document.getElementById((i + 1).toString());
         button.style.backgroundColor = "green";
     }
-    if (button.backgroundColor="green") {
-        button.href = 'cancel.html';
-        }
 
 }
+
 
 
 
